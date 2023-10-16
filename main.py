bd7f57ec88a5749af2d08c121317a654e87c95a3
ilyagenius <120258306+ilyagenius@users.noreply.github.com>
2023-10-16 18:11:47 +0300

    Update_with_CHATGPT main.py

--- main.py
+++ main.py
@@ -2,49 +2,27 @@ import discord
 from discord.ext import commands
 import bot_logic
 from bot_logic import *
-
+import openai
 
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+openai.api_key = 'enter your token here'
 
-bot = commands.Bot(command_prefix='?', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#     if message.content.startswith('$hello'):
-#         await message.channel.send("Hi!")
-#     elif message.content.startswith('$bye'):
-#         await message.channel.send("\\U0001f642")
-#     else:
-#         await message.channel.send(message.content)
-#
-# @bot.event
-# async def on_message(message):
-#     if message.content.startswith('Genpass'):
-#         await message.channel.send(gen_pass(10))
-#
-# @bot.event
-# async def on_message(message):
-#     if message.content.startswith('Createemoji'):
-#         await message.channel.send(gen_emodji())
 
-# @bot.event
-# async def on_message(message):
-#     if message.content.startswith('Flipcoin'):
-#         await message.channel.send(flip_coin())
-        
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
@@ -53,5 +31,19 @@ async def repeat(ctx, times: int, content='repeating...'):
         await ctx.send(content)
 
 
+@bot.command(name='gpt')
+async def cont(ctx: commands.context, *, args):
+    result = str(args)
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=result,
+    temperature=0.9,
+    max_tokens=1000,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=["You:"]
+)
+    await ctx.send(embed=discord.Embed(title=f'{result}', description=response['choices'][0]['text']))
 
-bot.run("")
+bot.run("Enter your token here")
