437155081f32e5e6976b6ca7056fd383616eb991
ilyagenius <120258306+ilyagenius@users.noreply.github.com>
2023-10-21 00:09:27 +0300

    Update_with_weather main.py
    You can find out the weather now using the /getweather

--- main.py
+++ main.py
@@ -1,29 +1,31 @@
+import json
 import discord
 from discord.ext import commands
-import bot_logic
-from bot_logic import *
 import openai
-
+import requests
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
-openai.api_key = 'enter your token here'
+openai.api_key = 'OpenaiApi token'
+Token = 'OpenWeathermap token'
 
 
-
+    # Функция говорит под каким user зарегистрировался бот
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 
+    # Функция узнает когда вступил участник
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
 
+    # Функция повторяет какую-либо строку некоторое количество раз
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
@@ -31,8 +33,10 @@ async def repeat(ctx, times: int, content='repeating...'):
         await ctx.send(content)
 
 
+    # Функция ответа как chatgpt(text-davinci-003)
 @bot.command(name='gpt')
 async def cont(ctx: commands.context, *, args):
+    """answers like chatgpt"""
     result = str(args)
     response = openai.Completion.create(
     model="text-davinci-003",
@@ -46,4 +50,16 @@ async def cont(ctx: commands.context, *, args):
 )
     await ctx.send(embed=discord.Embed(title=f'{result}', description=response['choices'][0]['text']))
 
-bot.run("Enter your token here")
+
+    # Функция для получения погоды
+@bot.command(name='getweather')
+async def getweather(ctx, *, city: str):
+    """The weather"""
+    city_name = city
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={Token}&units=metric')
+    data = json.loads(res.text)
+    await ctx.send(f'Погода в {city_name} - {data["main"]["temp"]}')
+
+
+
+bot.run("Token")
