a38baa329fa7df3e8b10204c46650bb7897141cb
ilyagenius <120258306+ilyagenius@users.noreply.github.com>
2023-10-21 11:35:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import json
+import random
+
 import discord
 from discord.ext import commands
 import openai
 import requests
+import os
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='/', intents=intents)
-openai.api_key = 'OpenaiApi token'
-Token = 'OpenWeathermap token'
+openai.api_key = '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'
+Token = 'd09550d89a3970a7e9f56a0935d2a645'
 
 
-    # Функция говорит под каким user зарегистрировался бот
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 
-    # Функция узнает когда вступил участник
 @bot.command()
 async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
 
-    # Функция повторяет какую-либо строку некоторое количество раз
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
     """Repeats a message multiple times."""
@@ -33,14 +34,13 @@ async def repeat(ctx, times: int, content='repeating...'):
         await ctx.send(content)
 
 
-    # Функция ответа как chatgpt(text-davinci-003)
 @bot.command(name='gpt')
 async def cont(ctx: commands.context, *, args):
     """answers like chatgpt"""
     result = str(args)
     response = openai.Completion.create(
     model="text-davinci-003",
-    prompt=result,
+    prompt= result,
     temperature=0.9,
     max_tokens=1000,
     top_p=1.0,
@@ -51,15 +51,29 @@ async def cont(ctx: commands.context, *, args):
     await ctx.send(embed=discord.Embed(title=f'{result}', description=response['choices'][0]['text']))
 
 
-    # Функция для получения погоды
 @bot.command(name='getweather')
 async def getweather(ctx, *, city: str):
-    """The weather"""
+    """The weather in Moscow"""
     city_name = city
     res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={Token}&units=metric')
     data = json.loads(res.text)
     await ctx.send(f'Погода в {city_name} - {data["main"]["temp"]}')
 
 
+@bot.command(name='recycle')
+async def cont(ctx: commands.context, *, args):
+    """answers like chatgpt"""
+    result = str(args)
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt= f'Перерабатывается{result}',
+    temperature=0.9,
+    max_tokens=1000,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=["You:"]
+)
+    await ctx.send(embed=discord.Embed(title=f'{result}', description=response['choices'][0]['text']))
 
-bot.run("Token")
+bot.run("MTE2MDk5MDY1MjMwMjEwNjY2NA.G08nan.daQ2giBhhk4UWDxAeJHoZ5tDqq96wUMDRivL4Y")
